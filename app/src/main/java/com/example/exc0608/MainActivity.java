2bed6f9c09daccf6000f54ba75bbca3999c44323
dz2244 <dz2244@bs.amalnet.k12.il>
2024-02-19 16:17:56 +0200

    last commit

--- app/src/main/java/com/example/exc0608/MainActivity.java
+++ app/src/main/java/com/example/exc0608/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.exc0608;
 
+import static java.lang.Math.round;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,7 +15,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv1,tv2,tv3,tv4,tv5,tv6;
-    int rand1,rand2,rand3,rand4,ans1,ans2,ans3;
+    int rand1,rand2,rand3,rand4,ans1,ans2,ans3,counter;
     String str1,str2,str3;
     EditText etn1,etn2,etn3;
     ImageView iv1,iv2,iv3;
@@ -61,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         tv6.setVisibility(View.INVISIBLE);
         etn2.setVisibility(View.INVISIBLE);
         etn3.setVisibility(View.INVISIBLE);
+        iv1.setVisibility(View.INVISIBLE);
         iv2.setVisibility(View.INVISIBLE);
         iv3.setVisibility(View.INVISIBLE);
         btn2.setVisibility(View.INVISIBLE);
@@ -75,52 +78,82 @@ public class MainActivity extends AppCompatActivity {
         str1 = etn1.getText().toString();
         if (!str1.isEmpty()) {
             ans1 = Integer.parseInt(str1);
+            iv1.setVisibility(View.VISIBLE);
             if (ans1 == rand1 + rand2)
+            {
                 iv1.setImageResource(R.drawable.check);
+                counter++;
+            }
             else
                 iv1.setImageResource(R.drawable.x);
 
             tv3.setVisibility(View.VISIBLE);
             tv4.setVisibility(View.VISIBLE);
             etn2.setVisibility(View.VISIBLE);
-            iv2.setVisibility(View.VISIBLE);
             btn2.setVisibility(View.VISIBLE);
             tv3.setText("" + (rand1 + rand2));
         }
     }
-
-
     public void clickedEtn2(View view) {clickedEtn2 = true;}
-
-
     public void clickedBtn2(View view) {
         str2 = etn2.getText().toString();
         if (!str2.isEmpty()) {
             ans2 = Integer.parseInt(str2);
+            iv2.setVisibility(View.VISIBLE);
             if (ans2 == rand3 + rand1 + rand2)
+            {
                 iv2.setImageResource(R.drawable.check);
+                counter++;
+            }
             else
                 iv2.setImageResource(R.drawable.x);
 
             tv5.setVisibility(View.VISIBLE);
             tv6.setVisibility(View.VISIBLE);
             etn3.setVisibility(View.VISIBLE);
-            iv3.setVisibility(View.VISIBLE);
             btn3.setVisibility(View.VISIBLE);
             tv5.setText("" + (rand3 + rand1 + rand2));
         }
     }
-
     public void clickedEtn3(View view) {clickedEtn3 = true;}
 
     public void clickedBtn3(View view) {
         str3 = etn3.getText().toString();
         if (!str3.isEmpty()) {
             ans3 = Integer.parseInt(str3);
+            iv3.setVisibility(View.VISIBLE);
             if (ans3 == rand4 + rand1 + rand2 + rand3)
+            {
                 iv3.setImageResource(R.drawable.check);
+                counter++;
+            }
             else
                 iv3.setImageResource(R.drawable.x);
+        bigbtn.setText(round(counter * 33.333 )+"% ," + counter + "/3");
         }
+    bigbtn.setVisibility(View.VISIBLE);
+    }
+    public void clickedBigbtn(View view) {
+        etn1.setText("");
+        etn2.setText("");
+        etn3.setText("");
+        tv3.setVisibility(View.INVISIBLE);
+        tv4.setVisibility(View.INVISIBLE);
+        tv5.setVisibility(View.INVISIBLE);
+        tv6.setVisibility(View.INVISIBLE);
+        etn2.setVisibility(View.INVISIBLE);
+        etn3.setVisibility(View.INVISIBLE);
+        iv1.setVisibility(View.INVISIBLE);
+        iv2.setVisibility(View.INVISIBLE);
+        iv3.setVisibility(View.INVISIBLE);
+        btn2.setVisibility(View.INVISIBLE);
+        btn3.setVisibility(View.INVISIBLE);
+        bigbtn.setVisibility(View.INVISIBLE);
+        rand1 = rnd.nextInt(90)+10;
+        rand2 = rnd.nextInt(90)+10;
+        tv1.setText(" "+rand1);
+        tv2.setText(" "+rand2);
+        bigbtn.setText(" "+rand2);
+        counter = 0;
     }
 }
